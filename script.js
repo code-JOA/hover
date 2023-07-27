a7e53e0aa871220bb3a3fe551e62430ce8d18a14
Joshua Owusu <joshbillion1@gmail.com>
2023-07-27 17:42:26 +0000

    Commit auto_update

---
+++ script.js
@@ -0,0 +1,59 @@
+var circle = document.querySelector("#circle");
+var frame = document.querySelector(".frame");
+
+const lerp = (x, y, a) => x * (1 - a) + y * a;
+
+// window.addEventListener("mousemove", function (dets) {
+//   circle.style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`;
+// });
+
+window.addEventListener("mousemove", function (dets) {
+  gsap.to(circle, {
+    x: dets.clientX,
+    y: dets.clientY,
+    duration: 0.2,
+    ease: Expo,
+  });
+});
+
+frame.addEventListener("mousemove", function (dets) {
+  var dims = frame.getBoundingClientRect();
+
+  var xstart = dims.x;
+  var xend = dims.x + dims.width;
+
+  //   gsap.utils.clamp(xstart, xend);
+  var zeroone = gsap.utils.mapRange(xstart, xend, 0, 1, dets.clientX);
+
+  gsap.to(circle, {
+    scale: 8,
+  });
+
+  gsap.to(".frame span", {
+    color: "#fff",
+    duration: 0.2,
+    y: "-5vw",
+  });
+
+  gsap.to(".frame span", {
+    x: lerp(-50, 50, zeroone),
+    duration: 0.3,
+  });
+});
+
+frame.addEventListener("mouseleave", function (dets) {
+  gsap.to(circle, {
+    scale: 1,
+  });
+
+  gsap.to(".frame span", {
+    color: "#111",
+    duration: 0.4,
+    y: 0,
+  });
+
+  gsap.to(".frame span", {
+    x: 0,
+    duration: 0.3,
+  });
+});
